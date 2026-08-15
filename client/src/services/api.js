e1aac52c8e99c56168888caf3bd062b255cbbf52
Pratham Vishwakarma <prathamdhiman562@gmail.com>
2026-08-16 00:19:20 +0530

    Make frontend API URL dynamic for production deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create custom axios instance
 const api = axios.create({
-  baseURL: "/",
+  baseURL: import.meta.env.VITE_API_URL || "/",
   timeout: 30000, // 30 seconds
   headers: {
     "Content-Type": "application/json",
